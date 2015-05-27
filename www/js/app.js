c3987c22096b1df15b40c3e4be23a938df742b55
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-27 15:07:00 -0700

    Some file restructuring

--- www/js/app.js
+++ www/js/app.js
@@ -31,24 +31,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-
   .state('app.sessions', {
     url: "/sessions",
     views: {
