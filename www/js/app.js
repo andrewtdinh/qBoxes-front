68f1c3d6fcbfa389534fa26661b1d99592d24962
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-28 14:24:26 -0700

    remove openFB related html files

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  // openFB.init({appId: '442668512567921'})
   $stateProvider
 
   .state('app', {
