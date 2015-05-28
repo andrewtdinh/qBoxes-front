0871c17614be11cae7eff28be2cd42a64fd9b385
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-27 23:56:53 -0700

    Add dummy user model

--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,29 @@
+'use strict';
+
 angular.module('starter.services', [])
-.factory('Session', function($resource){
+.factory('User', function($rootScope, $http, nodeUrl){
+  function User(){
+  }
+
+  User.initialize = function(){
+    return $http.post(nodeUrl + '/users');
+  };
+
+  User.oauth = function(provider){
+    return $rootScope.afAuth.$authWithOAuthPopup(provider);
+  };
+
+  User.register = function(user){
+    return $rootScope.afAuth.$createUser(user);
+  };
+
+  User.login = function(user){
+    return $rootScope.afAuth.$authWithPassword(user);
+  };
+
+  User.logout = function(){
+    return $rootScope.afAuth.$unauth();
+  };
+
+  return User;
 });
